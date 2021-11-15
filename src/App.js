0769da50065e9ba0f8a4000c63dee04161342ceb
Logan.Frye <Loganfrye714@gmail.com>
2021-11-15 08:56:41 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div> Logan Frye's - Technical Assessment Answers</div>
+        <button className="homepage__button">Question 1</button>
+        <button className="homepage__button">Question 2</button>
+        <button className="homepage__button">Question 3</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
