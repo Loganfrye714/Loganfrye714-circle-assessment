d5b8c5cfd82903f0dce82d58e5e75b16e56b563b
Logan.Frye <Loganfrye714@gmail.com>
2021-11-15 10:26:22 -0800

    question one done

--- src/Screens/questionOne.js
+++ src/Screens/questionOne.js
@@ -1,7 +1,20 @@
 import React from "react";
 
 function questionOne() {
-  return <div></div>;
+  function printHelloWorld() {
+    console.log("Hello World");
+  }
+  setInterval(printHelloWorld, 10000);
+
+  return (
+    <div className="App-header">
+      <h2>
+        Question - Create a function that logs “Hello World” to the console
+        every 10 seconds.
+      </h2>
+      <h2>Answer - Please view console</h2>
+    </div>
+  );
 }
 
 export default questionOne;
