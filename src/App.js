5df517e857fad72b008920b9d034fa7e86e8c8a2
Logan.Frye <Loganfrye714@gmail.com>
2021-11-15 10:04:49 -0800

    buttons to pages working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homescreen from "./Screens/homescreen";
+import QuestionOne from "./Screens/questionOne";
+import QuestonTwo from "./Screens/questonTwo";
+import QuestionThree from "./Screens/questionThree";
+import QuestionFour from "./Screens/questionThree";
 
 function App() {
   return (
@@ -8,6 +12,18 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Homescreen />} />
+        </Routes>
+        <Routes>
+          <Route path="/questionOne" element={<QuestionOne />} />
+        </Routes>
+        <Routes>
+          <Route path="/questionTwo" element={<QuestonTwo />} />
+        </Routes>
+        <Routes>
+          <Route path="/questionThree" element={<QuestionThree />} />
+        </Routes>
+        <Routes>
+          <Route path="/questionFour" element={<QuestionFour />} />
         </Routes>
       </BrowserRouter>
     </div>
--- src/Screens/homescreen.js
+++ src/Screens/homescreen.js
@@ -1,14 +1,23 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 function homescreen() {
   return (
     <div className="App">
       <header className="App-header">
         <div> Logan Frye's - Technical Assessment Answers</div>
-        <button className="homepage__button">Question 1</button>
-        <button className="homepage__button">Question 2</button>
-        <button className="homepage__button">Question 3</button>
-        <button className="homepage__button">Question 4</button>
+        <NavLink to="questionOne">
+          <button className="homepage__button">Question 1</button>
+        </NavLink>
+        <NavLink to="questionTwo">
+          <button className="homepage__button">Question 2</button>
+        </NavLink>
+        <NavLink to="questionThree">
+          <button className="homepage__button">Question 3</button>
+        </NavLink>
+        <NavLink to="questionFour">
+          <button className="homepage__button">Question 4</button>
+        </NavLink>
       </header>
     </div>
   );
